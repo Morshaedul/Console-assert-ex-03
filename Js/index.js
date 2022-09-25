ba1dfcbdc4a4987034763ff4b2243195702dc2e8
Morshaedul <morshaedulanirbanit@gmail.com>
2022-09-26 00:02:33 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,27 @@
+let x = 5;
+let y = 5;
+const myObj = {firsname:"John", lastname:"Doe"};
+console.assert(x + y == 11, myObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
